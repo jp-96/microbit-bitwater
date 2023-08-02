e375536cab24c6c05dc7cc2258f18f6d71f0f134
jp-96 <jp-96@outlook.jp>
2023-08-02 10:02:02 +0900

    拡張機能の追加

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
+mstate.defineStateDescription("初期化", ["容量と水量の初期化"], function (STATE) {
+    mstate.declareTransition(STATE, "アイドル", "初期化完了")
+})
+mstate.defineStateDescription("アイドル", ["水量の表示"], function (STATE) {
 	
+})
+mstate.exportUml("初期化", true, function (line) {
+    console.log(line)
 })
